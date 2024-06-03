c1cbe98ab4d2a6e782203023948330275ff17c1e
p1000-sec <78380370+p1000-sec@users.noreply.github.com>
2024-06-03 02:16:26 -0600

    Updated script.js

--- Website/script.js
+++ Website/script.js
@@ -30,3 +30,92 @@ $(document).ready(function() {
         }
     });
 });
+// Popup Form stuff
+$(document).ready(function() {
+    var popupForm = $('#popupForm');
+    var openFormBtn = $('#openFormBtn');
+    var closeBtn = $('.close-btn');
+    var chatBox = $('#chatBox');
+    var chatForm = $('#chatForm');
+    var chatInput = $('#chatInput');
+    var forumList = $('#forumList');
+    var chatContainer = $('#chatContainer');
+    var forumTitle = $('#forumTitle');
+    
+    // Sample forums data
+    var forums = [
+        { id: 1, title: "General Discussion" },
+        { id: 2, title: "Music Talk" },
+        { id: 3, title: "Event Planning" },
+    ];
+    
+    var currentForumId = null;
+
+    openFormBtn.on('click', function() {
+        popupForm.show();
+        loadForumList();
+    });
+
+    closeBtn.on('click', function() {
+        popupForm.hide();
+        chatContainer.hide();
+        forumList.show();
+    });
+
+    $(window).on('click', function(event) {
+        if ($(event.target).is(popupForm)) {
+            popupForm.hide();
+        }
+    });
+
+    chatForm.on('submit', function(event) {
+        event.preventDefault();
+        var message = chatInput.val().trim();
+        if (message) {
+            // Save the message to the server
+            saveMessage(currentForumId, message);
+
+            chatBox.append('<div>' + message + '</div>');
+            chatInput.val('');
+            chatBox.scrollTop(chatBox[0].scrollHeight);
+        }
+    });
+
+    function loadForumList() {
+        forumList.empty();
+        forums.forEach(function(forum) {
+            var button = $('<button></button>')
+                .text(forum.title)
+                .attr('data-id', forum.id)
+                .on('click', function() {
+                    var forumId = $(this).data('id');
+                    loadChat(forumId);
+                });
+            forumList.append(button);
+        });
+    }
+
+    function loadChat(forumId) {
+        currentForumId = forumId;
+        var forum = forums.find(f => f.id === forumId);
+        forumTitle.text(forum.title);
+
+        // Fetch messages from the server
+        fetchMessages(forumId);
+
+        chatContainer.show();
+        forumList.hide();
+    }
+
+    function fetchMessages(forumId) {
+        // Replace with server-side call
+        // For now, we'll just clear the chat box
+        chatBox.empty();
+    }
+
+    function saveMessage(forumId, message) {
+        // Replace with server-side call
+        // For now, we'll just log the message
+        console.log("Saving message:", { forumId, message });
+    }
+});
