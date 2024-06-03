46490bddeb2e319fe606a14258fb05f4e3d01c68
p1000-sec <78380370+p1000-sec@users.noreply.github.com>
2024-06-02 19:11:14 -0600

    Initial Website Files Added

---
+++ Website/script.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {  
+    $("#entryLogo, #entryBG, #entryText, #main").show()
+    $("#scrollingBG").fadeIn(1500)
+    $("#bg1").delay(2000).fadeIn(2000)
+    $("#scrollingBG2").delay(4000).animate({
+        left: '-1500px',
+    });
+    $("#scrollingBG3").delay(1500).fadeIn(2000)
+    $("#scrollingBG4").delay(3000).animate({
+        left: '600px',
+    });
+    $("#entryLogo").delay(6000).animate({
+        left: '780px',
+    });
+    $("#entryText").delay(6000).animate({
+        left: '25px',
+    });
+
+});
