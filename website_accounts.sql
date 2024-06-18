9a153a0e9e8500e4b24f478c1c1575aefb3ae65e
p1000-sec <78380370+p1000-sec@users.noreply.github.com>
2024-06-18 13:49:13 -0600

    More user info in website accounts database.

--- website_accounts.sql
+++ website_accounts.sql
@@ -1,4 +1,3 @@
-
 CREATE DATABASE website_accounts;
 
 USE website_accounts;
@@ -6,5 +5,10 @@ USE website_accounts;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    personal_info TEXT,
+    favorite_genres VARCHAR(255),
+    recent_listening_history TEXT,
+    user_type ENUM('premium', 'standard') DEFAULT 'standard',
+    chat_names TEXT
 );
