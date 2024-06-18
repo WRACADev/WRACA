0f10eea71320b7164fb33f43271fac042832e461
p1000-sec <78380370+p1000-sec@users.noreply.github.com>
2024-06-18 10:58:14 -0600

    Delete create_database.sql.py

--- create_database.sql.py
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE website_accounts;
-
-USE website_accounts;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
